614214d07b170006c3eb2779b2fb173639ffaabe
AhsanAzim <ahsan-azim@live.com>
2016-07-28 15:02:09 -0400

    facebook auth complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
 
 // example class based component (smart component)
 class App extends Component {
@@ -8,6 +9,18 @@ class App extends Component {
     this.state = {
 
     };
+    this.onFacebookLogin = this.onFacebookLogin.bind(this);
+  }
+
+
+  onFacebookLogin(response) {
+    if (response.status !== 'not_authorized' && response.status !== 'unknown') {
+      document.getElementById('facebook').innerHTML = `Welcome, ${response.name}! `;
+      document.getElementById('facebook').innerHTML += `<img src="${response.picture.data.url}"/>`;
+      document.getElementById('facebook').style = 'display: block';
+    } else {
+      document.getElementById('facebook').style = 'display: block';
+    }
   }
 
 
@@ -15,6 +28,14 @@ class App extends Component {
     // Add in FacebookLogin, GoogleLogin and FirebaseApp
     return (
       <div id="login">
+        <div id="facebook">
+          <FacebookLogin
+            appId="899434840201089"
+            autoLoad
+            fields="name,email,picture.type(large)"
+            callback={this.onFacebookLogin}
+          />
+        </div>
       </div>
     );
   }
